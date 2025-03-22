c4297723cd40f5d557c36108dfb572c28c743d33
wame <165180226+Wame69@users.noreply.github.com>
2025-03-22 09:40:45 +0700

    Update SQL_Base_9_Primer.sql

--- SQL_Base_9_Primer.sql
+++ SQL_Base_9_Primer.sql
@@ -1,26 +1,26 @@
--- Пример 1
-CREATE TABLE Управление1 (
+-- РџСЂРёРјРµСЂ 1
+CREATE TABLE РЈРїСЂР°РІР»РµРЅРёРµ1 (
     ID INT,
-    Вид VARCHAR(20)
+    Р’РёРґ VARCHAR(20)
 );
 
--- Пример 2
-DROP TABLE Управление1;
+-- РџСЂРёРјРµСЂ 2
+DROP TABLE РЈРїСЂР°РІР»РµРЅРёРµ1;
 
--- Пример 3
-CREATE TABLE Управление1 (
+-- РџСЂРёРјРµСЂ 3
+CREATE TABLE РЈРїСЂР°РІР»РµРЅРёРµ2 (
     ID INT UNIQUE,
-    Вид VARCHAR(20) NOT NULL
+    Р’РёРґ VARCHAR(20) NOT NULL
 );
 
--- Пример 4
-CREATE TABLE Управление1 (
+-- РџСЂРёРјРµСЂ 4
+CREATE TABLE РЈРїСЂР°РІР»РµРЅРёРµ3 (
     ID INT CHECK (ID < 200),
-    Вид VARCHAR(20) DEFAULT 'Президентская республика'
+    Р’РёРґ VARCHAR(20) DEFAULT 'РџСЂРµР·РёРґРµРЅС‚СЃРєР°СЏ СЂРµСЃРїСѓР±Р»РёРєР°'
 );
 
--- Пример 5
-CREATE TABLE Управление1 (
+-- РџСЂРёРјРµСЂ 5
+CREATE TABLE РЈРїСЂР°РІР»РµРЅРёРµ4 (
     ID INT PRIMARY KEY IDENTITY(5,3),
-    Вид VARCHAR(20)
+    Р’РёРґ VARCHAR(20)
 );
